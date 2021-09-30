0ebec6eaa4da4cdd743ee43a3165ae7d423a9e6f
flangham <finnian.langham@gmail.com>
2021-09-30 23:13:11 +1000

    Added comments

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,7 @@ interface Props {
   children: React.ReactNode;
 }
 
+// Wraps page content to add responsive padding
 export const Layout: React.FC<Props> = ({ children }) => (
   <div className="px-2 pt-4 pb-8 xs:px-4 lg:py-8 max-w-6xl m-auto">
     {children}
--- src/utilities/parseDateTime.ts
+++ src/utilities/parseDateTime.ts
@@ -1,3 +1,4 @@
+// Convert from YYYY-MM-DDT12:42:04.000Z format to DD/MM/YYYY
 export const parseDateTime = (datetime: string) => {
   const day = datetime.slice(8, 10);
   const month = datetime.slice(5, 7);
