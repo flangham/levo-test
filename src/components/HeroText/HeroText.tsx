79b168374d75341b28cee8d45177a41a45772478
flangham <finnian.langham@gmail.com>
2021-09-30 19:13:56 +1000

    Increased bottom margins

--- src/components/HeroText/HeroText.tsx
+++ src/components/HeroText/HeroText.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 export default function HeroText() {
   return (
     <>
-      <h1 className="text-2xl font-bold mb-2">Lorem, ipsum dolor</h1>
-      <p className="mb-2">
+      <h1 className="text-2xl font-bold mb-8">Lorem, ipsum dolor</h1>
+      <p className="mb-8">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, laborum!
         Magni labore, cupiditate nulla ullam voluptates id doloremque aliquid
         dolore laudantium consequuntur quisquam, eos, incidunt doloribus libero
