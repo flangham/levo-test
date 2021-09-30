15e03508939d57acf06d840fe7f56d2d25ab5802
flangham <finnian.langham@gmail.com>
2021-09-30 19:40:01 +1000

    Articles now load in groups of a specified number, rather than 6 and then everything else

--- src/components/ArticlesGrid/ArticlesGrid.tsx
+++ src/components/ArticlesGrid/ArticlesGrid.tsx
@@ -11,8 +11,12 @@ interface ArticleFields {
 
 export const ArticlesGrid: React.FC = () => {
   const [articles, setArticles] = useState<ArticleFields[]>([]);
-  // TODO: Display in groups of 6
-  const [isShowingAll, setIsShowingAll] = useState<boolean>(false);
+
+  // Number of articles which are shown by default,
+  // and added each time you 'Load more'
+  const articlesPerLoad: number = 6;
+
+  const [articlesLoaded, setArticlesLoaded] = useState<number>(articlesPerLoad);
 
   useEffect(() => {
     fetch('https://api.spaceflightnewsapi.net/v3/articles')
@@ -26,27 +30,24 @@ export const ArticlesGrid: React.FC = () => {
   return (
     <>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
-        {isShowingAll
-          ? // Shows all if 'Load more' button clicked
-            articles.map((article) => (
-              <ArticleTile key={article.id} article={article} />
-            ))
-          : // Only show 6 by default
-            articles
-              .slice(0, 6)
-              .map((article) => (
-                <ArticleTile key={article.id} article={article} />
-              ))}
+        {
+          // Only show the number of articles that have been 'loaded'
+          articles.slice(0, articlesLoaded).map((article) => (
+            <ArticleTile key={article.id} article={article} />
+          ))
+        }
       </div>
-      {/* Button hides once clicked */}
-      {!isShowingAll && (
-        <button
-          onClick={() => setIsShowingAll(true)}
-          className="bg-gray-400 border border-black m-auto block p-2 hover:bg-black hover:text-white transition"
-        >
-          Load more
-        </button>
-      )}
+      {
+        // Hide button once all articles are displayed
+        articlesLoaded < articles.length && (
+          <button
+            onClick={() => setArticlesLoaded((prev) => prev + articlesPerLoad)}
+            className="bg-gray-400 border border-black m-auto block p-2 hover:bg-black hover:text-white transition"
+          >
+            Load more
+          </button>
+        )
+      }
     </>
   );
 };
