56df76321db12ba8b711e537828483bcb4bfe913
flangham <finnian.langham@gmail.com>
2021-09-30 23:50:34 +1000

    don't need to define interface just for children props

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,7 @@
-import { FC, ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
+import { FC } from 'react';
 
 // Wraps page content to add responsive padding
-export const Layout: FC<Props> = ({ children }) => (
+export const Layout: FC = ({ children }) => (
   <div className="px-2 pt-4 pb-8 xs:px-4 lg:py-8 max-w-6xl m-auto">
     {children}
   </div>
