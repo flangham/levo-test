c6e9e2af5de43b25014521eeb56eefafbdb71d83
flangham <finnian.langham@gmail.com>
2021-09-30 19:06:31 +1000

    Increased y-padding at larger breakpoint

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export const Layout: React.FC<Props> = ({ children }) => (
-  <div className="p-2 max-w-4xl m-auto">{children}</div>
+  <div className="p-2 lg:py-8 max-w-4xl m-auto">{children}</div>
 );
