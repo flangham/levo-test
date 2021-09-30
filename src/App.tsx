eb64c810a273159b864b157ef3f979ebff39fc40
flangham <finnian.langham@gmail.com>
2021-09-30 18:12:17 +1000

    added herotext

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/global.css';
 import { ArticlesGrid } from './components/ArticlesGrid/ArticlesGrid';
+import HeroText from './components/HeroText/HeroText';
 
 function App() {
   return (
     <>
+      <HeroText />
       <ArticlesGrid />
     </>
   );
---
+++ src/components/HeroText/HeroText.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function HeroText() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-2">Lorem, ipsum dolor</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, laborum!
+        Magni labore, cupiditate nulla ullam voluptates id doloremque aliquid
+        dolore laudantium consequuntur quisquam, eos, incidunt doloribus libero
+        nam ipsum asperiores?
+      </p>
+    </>
+  );
+}
